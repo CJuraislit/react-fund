d6ef9e0458ee1e5d120ab24cbc68c2cf56858392
CJ laptop <korolevpavel02@gmial.com>
2024-04-10 15:09:42 +0300

    list modified by using props from array

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import "./styles/App.css";
 import PostItem from "./components/PostItem";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript 1", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
+
   return (
     <>
       <div className="App">
-        <PostItem post={{ id: 1, title: "JavaScript", body: "Description" }} />
-        <PostItem post={{ id: 2, title: "JavaScript", body: "Description" }} />
-        <PostItem post={{ id: 3, title: "JavaScript", body: "Description" }} />
+        <h1 style={{ textAlign: "center" }}>Список постов</h1>
+        {posts.map((post) => (
+          <PostItem post={post} key={post.id} />
+        ))}
       </div>
     </>
   );
