4d304f7ca7c84edf4f0b166d09bd9e2f3f505a6e
CJ laptop <korolevpavel02@gmial.com>
2024-04-11 15:24:37 +0300

    Упарвляемый и неуправляемый компонет input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import "./styles/App.css";
@@ -20,12 +20,32 @@ function App() {
     { id: 3, title: "Python 3", body: "Description" },
   ]);
 
+  const [title, setTitle] = useState("");
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  };
+
   return (
     <>
       <div className="App">
         <form action="">
-          <MyInput value={title} type="text" placeholder="Название поста" />
-          <MyInput type="text" placeholder="Описание поста" />
+          {/* Управляемый компонент */}
+          <MyInput
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            placeholder="Название поста"
+          />
+          {/* Неуправляемый\Неконтролируемый компонет */}
+          <MyInput
+            ref={bodyInputRef}
+            type="text"
+            placeholder="Описание поста"
+          />
           <MyButton onClick={addNewPost}>Создать пост</MyButton>
         </form>
         <PostList posts={posts} title={"Посты про JS"} />
