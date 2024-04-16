ab8f701b3965ab3daa6f47694cd38ef504abeee2
CJ laptop <korolevpavel02@gmial.com>
2024-04-16 03:17:54 +0300

    Авторизация & глобальные данные useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Navbar from "./components/UI/navbar/Navbar";
 import AppRouter from "./components/AppRouter";
+import { AuthContext } from "./context";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      setIsAuth(true);
+    }
+    setisLoading(false);
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <AppRouter />
-      </BrowserRouter>
+      <AuthContext.Provider
+        value={{
+          isAuth,
+          setIsAuth,
+          isLoading,
+        }}
+      >
+        <BrowserRouter>
+          <Navbar />
+          <AppRouter />
+        </BrowserRouter>
+      </AuthContext.Provider>
     </>
   );
 }
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
 import { publicRoutes, privateRoutes } from "../router/routes";
+import { AuthContext } from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-  const isAuth = true;
+  const { isAuth, isLoading } = useContext(AuthContext);
+  console.log(isAuth);
+
+  if (isLoading) {
+    return <Loader />;
+  }
   return (
     <>
       {isAuth ? (
